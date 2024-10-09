171e9d02dec146d9eac3b02abb4aac4a5e6d5102
jaden-kwan <105980272+jaden-kwan@users.noreply.github.com>
2024-10-08 22:11:28 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hey, I'm <span className='text-[#915EFF]'>Jaden</span> :)
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            a passionate student in the STEM Program <br className='sm:block hidden' />
+            at Unionville High School
           </p>
         </div>
       </div>
